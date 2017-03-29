3442727f85fe491b2107b015f238bd889be1533c
Paul Rigge <rigge@berkeley.edu>
2017-03-28 23:23:38 -0700

    Add annotations for controlling analog verilog emission

--- src/test/scala/chiselTests/AnalogSpec.scala
+++ src/test/scala/chiselTests/AnalogSpec.scala
@@ -3,6 +3,7 @@
 package chiselTests
 
 import chisel3._
+import chisel3.experimental.ChiselAnnotation
 import chisel3.util._
 import chisel3.testers.BasicTester
 import chisel3.experimental.{Analog, attach}
@@ -18,9 +19,10 @@ class AnalogWriterIO extends Bundle {
   val in = Input(UInt(32.W))
 }
 
-trait AnalogReader {
+trait AnalogReader extends AnalogAnnotator {
   self: Module =>
   final val io = self.IO(new AnalogReaderIO)
+  analog(io.bus, "busbus")
 }
 
 class AnalogReaderBlackBox extends BlackBox with AnalogReader
@@ -41,6 +43,88 @@ class AnalogConnector extends Module {
   io.bus1 <> io.bus2
 }
 
+// import chisel3.internal.InstanceId
+import firrtl.ir.{AnalogType, Circuit, DefModule, Expression, HasName, Port, Statement, Type}
+import firrtl.{CircuitForm, CircuitState, LowForm, Transform}
+import firrtl.annotations.{Annotation, ModuleName, Named, ComponentName}
+import firrtl.Mappers._
+
+object AnalogVerilogTpeAnnotation {
+  def apply(target: Named, value: String): Annotation =
+    Annotation(target, classOf[AnalogVerilogTpeAnnotation], value)
+
+  def unapply(a: Annotation): Option[(ComponentName, String)] = a match {
+    case Annotation(named, t, value) if t == classOf[AnalogVerilogTpeAnnotation] => named match {
+      case c: ComponentName => Some((c, value))
+      case _ => None
+    }
+    case _ => None
+  }
+}
+
+class AnalogVerilogTpeAnnotation extends Transform {
+  override def inputForm: CircuitForm  = LowForm
+  override def outputForm: CircuitForm = LowForm
+
+  override def execute(state: CircuitState): CircuitState = {
+    getMyAnnotations(state) match {
+      case Nil => state
+      case annos =>
+        val analogs = annos.collect { case AnalogVerilogTpeAnnotation(ana, name) => (ana, name) }
+        println(s"All the analogs are $analogs")
+        state.copy(circuit = run(state.circuit, analogs))
+    }
+  }
+
+  def run(circuit: Circuit, annos: Seq[(ComponentName, String)]): Circuit = {
+    circuit map walkModule(annos)
+  }
+  def walkModule(annos: Seq[(ComponentName, String)])(m: DefModule): DefModule = {
+    val filteredAnnos = Map(annos.filter(a => a._1.module.name == m.name).map {
+      case (c, s) => c.name.replace(".", "_") -> s
+    }: _*)
+    println(s"The filtered annos are $filteredAnnos in module ${m.name}")
+    m map walkStatement(filteredAnnos) map walkPort(filteredAnnos)
+  }
+  def walkStatement(annos: Map[String, String])(s: Statement): Statement = {
+    s map walkExpression(annos)
+  }
+  def walkPort(annos: Map[String, String])(p: Port): Port = {
+    println(s"Visited port $p with name ${p.name}, annos.contains(p.name) = ${annos.contains(p.name)}")
+    if (annos.contains(p.name)) {
+      updateAnalogVerilog(annos(p.name))(p.tpe)
+    }
+    p
+  }
+  def walkExpression(annos: Map[String, String])(e: Expression): Expression = {
+    println(s"Visited expression $e")
+    e match {
+      case h: HasName =>
+        println("Expression $e has name ${h.name}, annos.contains(h.name) = ${annos.contains(h.name)}")
+        if (annos.contains(h.name)) e mapType updateAnalogVerilog(annos(h.name))
+      case _ =>
+    }
+    e
+  }
+  def updateAnalogVerilog(value: String)(tpe: Type): Type = {
+    println(s"Updating analog verilog on $tpe to $value")
+    tpe match {
+      case a: AnalogType =>
+        println(s"Setting $a to $value")
+        a.verilogTpe = value
+        a
+      case t => t
+    }
+  }
+}
+
+trait AnalogAnnotator { self: Module =>
+  def analog(component: Analog, value: String): Unit = {
+    annotate(ChiselAnnotation(component, classOf[AnalogVerilogTpeAnnotation], value))
+  }
+}
+
+
 // Parent class for tests connecing up AnalogReaders and AnalogWriters
 abstract class AnalogTester extends BasicTester {
   final val BusValue = "hdeadbeef".U
@@ -57,6 +141,18 @@ abstract class AnalogTester extends BasicTester {
 
 class AnalogSpec extends ChiselFlatSpec {
   behavior of "Analog"
+
+  it should "do something with annotations" in {
+    Driver.execute(Array("--target-dir", "test_run_dir"), () => new AnalogReaderWrapper) match {
+      case ChiselExecutionSuccess(Some(circuit), emitted, _) =>
+        val annos = circuit.annotations
+        annos.length should be (2)
+
+      case _ =>
+        assert(false)
+      }
+  }
+
 
   it should "NOT be bindable to registers" in {
     a [ChiselException] should be thrownBy {
